195fb0fd899f87295a57cf3be958c41587036a6f
Njuguna-JohnBrian <njugunajb96@gmail.com>
2021-12-07 06:20:20 +0300

    Add User Route

---
+++ ServerSide/Userservice/routes/user.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllUsers,
+  getOneUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/users.controllers");
+
+router.route("/").get(getAllUser);
+router.route("/:id").get(getOneUser);
+router.route("/update/:id").put(updateUser);
+router.route("/delete/:id").delete(deleteUser);
