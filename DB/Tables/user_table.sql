c32dd5b830ec0455aae68c1ea90a658a3bba522b
Njuguna-JohnBrian <njugunajb96@gmail.com>
2021-12-08 11:00:06 +0300

    Update Table Users

--- DB/Tables/user_table.sql
+++ DB/Tables/user_table.sql
@@ -1,10 +1,12 @@
 -- Create table users:
-CREATE TABLE Users(
+CREATE TABLE Users
+(
     id INT PRIMARY KEY IDENTITY,
     email VARCHAR(100) NOT NULL UNIQUE,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(250) NOT NULL,
     phonenumber VARCHAR(20) NOT NULL,
-    role VARCHAR(20) NOT NULL,
-    is_deleted BIT DEFAULT 0 NOT NULL,
+    is_admin BIT DEFAULT 0 NOT NULL,
+    is_deleted BIT DEFAULT 0 NOT NULL
 )
+
