809c5c8ea4c6bf37b6a1e468ab55983ec73bf355
Njuguna-JohnBrian <njugunajb96@gmail.com>
2021-12-20 10:40:51 +0300

    Update auth service

---
+++ ClientSide/user-system/src/services/auth.service.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+//User Authentication service
+class AuthService {
+  login(username, password) {
+    return axios
+      .post("/user/login", {
+        username,
+        password,
+      })
+      .then((response) => {
+        if (response.data.accessToken) {
+          localStorage.setItem("user", JSON.stringify(response.data));
+        }
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+  }
+
+  register(username, email, password) {
+    return axios.post("/user/signup", {
+      username,
+      email,
+      password,
+    });
+  }
+
+  getCurrentUser() {
+    return JSON.parse(localStorage.getItem("user"));
+  }
+}
+
+export default new AuthService();
