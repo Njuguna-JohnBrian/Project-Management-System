75659fe616ce026f2bc0875264a01200db9156c2
Njuguna-JohnBrian <njugunajb96@gmail.com>
2021-12-20 09:36:51 +0300

    Update service worker and index

--- ClientSide/user-system/src/index.js
+++ ClientSide/user-system/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
-reportWebVitals();
+serviceWorker.unregister();
