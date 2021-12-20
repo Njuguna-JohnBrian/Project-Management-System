be9d6b85ba862f02d32a39b39c0aba10db305bd4
Njuguna-JohnBrian <njugunajb96@gmail.com>
2021-12-20 09:39:43 +0300

    Import Bootstrap

--- ClientSide/user-system/src/App.js
+++ ClientSide/user-system/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return <div>Jb</div>;
   }
