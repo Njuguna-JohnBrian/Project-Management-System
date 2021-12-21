70c0d672d50a4bdedc1d19480157acb34381a4d4
Njuguna-JohnBrian <njugunajb96@gmail.com>
2021-12-21 10:27:43 +0300

    Update Stored Procedures

--- DB/Stored Procedures/Users Procs/create.user.proc.sql
+++ DB/Stored Procedures/Users Procs/create.user.proc.sql
@@ -1,9 +1,9 @@
-CREATE PROCEDURE createUser (
+CREATE OR ALTER PROCEDURE createUser (
     @email AS VARCHAR(100),
     @username AS VARCHAR(50),
-    @password VARCHAR(250),
-    @phonenumber VARCHAR(20),
-    @role VARCHAR(20)
+    @password VARCHAR(500),
+    @phonenumber VARCHAR(20)
+   
 ) AS 
 
 BEGIN 
@@ -11,18 +11,24 @@ BEGIN
         email,
         username,
         password,
-        phonenumber,
-        role
+        phonenumber
     )
     VALUES(
         @email,
         @username,
         @password,
-        @phonenumber,
-        @role
+        @phonenumber
+        
     );
 END;
 
-EXECUTE createUser "njugunajohnbrian@gmail.com", "Njuguna", "12345","07123456", "User";
+-- EXECUTE createUser "njugunajohnbrian@gmail.com", "Njuguna", "12345","07123456", "User";
 
 SELECT * FROM Users
+
+-- Update rows in table '[Users]' in schema '[dbo]'
+UPDATE [dbo].[Users]
+SET
+    is_admin=1
+    -- Add more columns and values here
+WHERE is_deleted=0;
--- DB/Tables/user_table.sql
+++ DB/Tables/user_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE Users
     id INT PRIMARY KEY IDENTITY,
     email VARCHAR(100) NOT NULL UNIQUE,
     username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(250) NOT NULL,
+    password VARCHAR(500) NOT NULL,
     phonenumber VARCHAR(20) NOT NULL,
     is_admin BIT DEFAULT 0 NOT NULL,
     is_deleted BIT DEFAULT 0 NOT NULL
