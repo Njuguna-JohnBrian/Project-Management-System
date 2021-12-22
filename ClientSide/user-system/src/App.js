23d00eaa4c65708c040b0b34737f2b31b7230a45
Njuguna-JohnBrian <njugunajb96@gmail.com>
2021-12-22 09:41:10 +0300

    Add home route

--- ClientSide/user-system/src/App.js
+++ ClientSide/user-system/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css"
+import "./App.css";
 
 // import AuthService from "./services/auth.service";
 import Login from "./components/login.component";
 import Register from "./components/register.component";
+import Home from "./components/home.component";
 
 class App extends Component {
   render() {
@@ -15,8 +16,15 @@ class App extends Component {
           <Link to={"/"} className="navbar-brand">
             USER SYSTEM
           </Link>
+
           {/* Add Admin and User DashBoards */}
           <div className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link to={"/home"} className="nav-link">
+                Home
+              </Link>
+            </li>
+
             <li className="nav-item">
               <Link to={"/login"} className="nav-link">
                 Login
@@ -33,6 +41,7 @@ class App extends Component {
 
         <div className="container mt-3">
           <Switch>
+            <Route exact path={["/", "/home"]} component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
           </Switch>
