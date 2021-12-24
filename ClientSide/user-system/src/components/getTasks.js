cd55f5b044cb291d1c545b85650d46ab4c5faf08
Njuguna-JohnBrian <njugunajb96@gmail.com>
2021-12-24 10:47:44 +0300

    Get all tasks in a project

--- ClientSide/user-system/src/components/getTasks.js
+++ ClientSide/user-system/src/components/getTasks.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import React from "react";
-
+import { Table } from "react-bootstrap";
+import * as Icon from "react-bootstrap-icons";
 
 export default class GetTasks extends React.Component {
   state = {
     id: "",
+    taskItems: [],
   };
 
   handleChange = (e) => {
@@ -17,10 +19,16 @@ export default class GetTasks extends React.Component {
     axios
       .get(`http://localhost:9000/tasks/tasks/${this.state.id}`)
       .then((res) => {
+        console.log(res.data);
+        const taskItems = res.data;
+        this.setState({ taskItems });
         return res.data;
       });
   };
   render() {
+    let getAllTasks = this.state.taskItems.map((taskItem) => {
+      return taskItem;
+    });
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
@@ -30,6 +38,32 @@ export default class GetTasks extends React.Component {
           </label>
           <button type="submit">Get Tasks</button>
         </form>
+        <Table striped bordered hover responsive="md">
+          <thead>
+            <tr>
+              <th>Project Id</th>
+              <th>Task Id</th>
+              <th>Task Name</th>
+              <th>Task Description</th>
+              <th>Status</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {getAllTasks.map((task) => (
+              <tr key={task.id}>
+                <td>{task.project_id}</td>
+                <td>{task.id}</td>
+                <td>{task.task_name}</td>
+                <td>{task.task_desc}</td>
+                <td>{task.is_deleted===true ? "Inactive" :"Active"}</td>
+                <td>
+                  <Icon.Trash color="red" size={20} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
     );
   }
