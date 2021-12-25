f74e7e9a32e2ca42b85b026303999e2bcf6486fa
Njuguna-JohnBrian <njugunajb96@gmail.com>
2021-12-25 14:48:37 +0300

    Create delete functionality

--- ClientSide/user-system/src/components/users.component.js
+++ ClientSide/user-system/src/components/users.component.js
@@ -6,6 +6,20 @@ import * as Icon from "react-bootstrap-icons";
 export default class GetAllUsers extends React.Component {
   state = {
     items: [],
+    userId: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({ userId: e.target.value });
+  };
+  handleDelete = (e) => {
+    e.preventDefault();
+
+    axios.delete(`/admin/delete/${this.state.userId}`).then((res) => {
+      const userId = res.data;
+      this.setState({ userId });
+      return res.data;
+    });
   };
   componentDidMount() {
     axios.get("/admin/all").then((res) => {
@@ -21,24 +35,36 @@ export default class GetAllUsers extends React.Component {
       <Table striped bordered hover responsive="md">
         <thead>
           <tr>
-            <th>User Id</th>
             <th>UserName</th>
             <th>Email</th>
             <th>Phone Number</th>
             <th>Active</th>
+            <th>User Id</th>
             <th>Delete</th>
           </tr>
         </thead>
         <tbody>
           {allUsers.map((user) => (
             <tr key={user.id}>
-              <td>{user.id}</td>
               <td>{user.username}</td>
               <td>{user.email}</td>
               <td>{user.phonenumber}</td>
               <td>{user.is_deleted == 0 ? "True" : "False"}</td>
+              <td>{user.id}</td>
               <td>
-                <Icon.Trash color="red" size={20} onClick={console.log("Unataka")}/>
+                {
+                  <form onSubmit={this.handleDelete}>
+                    <input
+                      type="number"
+                      name="id"
+                      onChange={this.handleChange}
+                    />
+
+                    <button type="submit">
+                      <Icon.Trash color="red" size={20} />
+                    </button>
+                  </form>
+                }
               </td>
             </tr>
           ))}
